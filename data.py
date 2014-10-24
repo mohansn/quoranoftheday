f25589671ac22fe1495032e1e5a7291483105ea3
Mohan Nayaka <mohannayaka.msccsc2@iiitmk.ac.in>
2014-10-24 08:31:36 +0530

    Improvements for mobile
    Implemented recommendation to configure viewport.
    https://developers.google.com/speed/pagespeed/insights/?url=quoran-of-the-day.appspot.com&tab=mobile
    https://developers.google.com/speed/docs/insights/ConfigureViewport

--- data.py
+++ data.py
@@ -7,6 +7,7 @@ part1="""
 <html>
   <head>
     <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta name=viewport content="width=device-width, initial-scale=1">
     <style>
       .title {
           text-align:center;
